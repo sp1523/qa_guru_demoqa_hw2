0895dca2b646cc687688c6a7536c6e9859cd6ba1
sp1523 <theftorm@gmail.com>
2022-09-26 18:48:30 +0300

    HW with video help2

--- src/test/java/com/demoqa/DemoQaHw.java
+++ src/test/java/com/demoqa/DemoQaHw.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
+
 
 
 public class DemoQaHw {
